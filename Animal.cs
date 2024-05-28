d203d72d3b8c2c1f6d81f08ce2e5d3cfa51356eb
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-05-28 21:57:50 +0500

    Create Method

---
+++ Animal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KESCHA
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public  int AgeDifference { get; set; }
+
+        public Animal(string name, int age) 
+        {
+            Name = name;
+            Age = age;
+        }
+        public Animal(
+            string name,
+            int userAge,
+            int animalAge)
+        {
+            Name = name;
+            Age = animalAge;
+            CalculateAgeDifferenceWithNoReturn(userAge, animalAge);
+        }
+        public void PrintAgeDifference()
+        {
+            Console.WriteLine($"The difference betwen your and {Name}' s age is {AgeDifference}");
+        }
+
+        public void Greet(string userName)
+        {
+            if(userName == "")
+            {
+                userName = "Unkown Person";
+            }
+            Console.WriteLine($"Hello {userName}");
+        }
+        public void CalculateAgeDifferenceWithNoReturn(int userAge)
+        {
+            AgeDifference =  userAge - Age;
+        }
+
+        public int CalculateAgeDifference(int userAge, int animalAge)
+        {
+            return userAge - animalAge;
+        }
+        public void CalculateAgeDifferenceWithNoReturn(int userAge, int animalAge)
+        {
+            AgeDifference = userAge - animalAge;
+        }
+
+        public void CompareAges(int userAge)
+        {
+            if(userAge > Age) 
+            {
+                Console.WriteLine("You are older!");
+            }
+            else if(userAge == Age)
+            {
+                Console.WriteLine("You are equal!");
+            }
+            else
+            {
+                Console.WriteLine("You are younger!");
+            }
+        }
+        public void TellAbuotFriends(string userName, int userAge)
+        {
+            Console.WriteLine("Let me tell you about my friends.");
+
+            string[] friendsName = new string[3];
+            friendsName[0] = "Kasee";
+            friendsName[1] = "Vasya";
+            friendsName[2] = userName;
+            int[] friendsAge = { 1, 2, userAge};
+            
+            for(int iterator = 0;  iterator < friendsName.Length; iterator++)
+            {
+                Console.WriteLine($"{friendsName[iterator]} is {friendsAge[iterator]} year old!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using KESCHA;
+
+Animal kescha = new Animal(name: "Kescha", age: 3);
+
+Console.Write("Enter your name: ");
+string userName = Console.ReadLine();
+kescha.Greet(userName);
+
+Console.Write("Enter you age: ");
+string ageAsString = Console.ReadLine();
+
+Console.WriteLine("Converting....");
+int userAge = Convert.ToInt32(ageAsString);
+Console.WriteLine($"Succesfully converted! {userAge}");
+
+kescha.CalculateAgeDifferenceWithNoReturn(userAge);
+kescha.PrintAgeDifference();
+kescha.CompareAges(userAge);
+kescha.TellAbuotFriends(userName, userAge);
